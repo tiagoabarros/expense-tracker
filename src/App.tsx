46ab39dfa061a76cf172b2086f637aadd70d118d
tiagoabarros <tiagoabarross@gmail.com>
2023-02-09 11:23:12 -0300

    creating the overall style structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import * as C from "./styles/App.styled";
+
 export const App = () => {
   return (
-    <div>Olá Mundo!</div>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Controle Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        Olá Mundo
+      </C.Body>
+    </C.Container>
   );
 };
